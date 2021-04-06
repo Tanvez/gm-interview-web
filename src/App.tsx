477228b722fd28d59cf96552db9887ffecad3d37
Vesna Tan <tanvezi@gmail.com>
2021-04-06 15:41:58 -0400

    commented out modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import BasicTable from './pages/Entries';
-import Modal from './components/modal'
+// import Modal from './components/modal'
 
 function App() {
   return (
