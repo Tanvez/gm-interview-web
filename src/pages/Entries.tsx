563bb4f754f6d7e0052d104fc7dabecd8b1d4b67
Vesna Tan <tanvezi@gmail.com>
2021-04-06 15:53:54 -0400

    dummy data block

--- src/pages/Entries.tsx
+++ src/pages/Entries.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import { useQuery, gql } from "@apollo/client";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { useQuery, gql } from '@apollo/client';
 
 const useStyles = makeStyles({
   table: {
@@ -18,35 +18,29 @@ const useStyles = makeStyles({
     borderColor: "lightgrey",
     borderWidth: "1px",
     fontWeight: "bold",
-    paddingBottom: "6px",
-    paddingTop: "6px",
-    color: "grey",
+    paddingBottom:'6px',
+    paddingTop:'6px',
+    color: 'grey'
   },
   tableCell: {
-    color: "#627bc5",
-    fontWeight: 600,
-  },
+    color: '#627bc5',
+    fontWeight: 600
+  }
 });
 
-function createData(
-  name: string,
-  clients: string,
-  hours: number,
-  billableHours: number,
-  billableAmount: number
-) {
+function createData(name: string, clients: string, hours: number, billableHours: number, billableAmount: number) {
   return { name, clients, hours, billableHours, billableAmount };
 }
 
 interface Entry {
-  client: string;
-  project: string;
-  projectCode: string;
-  hours: string;
-  firstName: string;
-  lastName: string;
-  billable: boolean;
-  billableRats: number;
+    client: string;
+    project: string;
+    projectCode: string;
+    hours: string;
+    firstName: string;
+    lastName: string;
+    billable: boolean;
+    billableRats: number;
 }
 
 interface EntriesData {
@@ -54,8 +48,9 @@ interface EntriesData {
 }
 
 const GET_ENTRIES = gql`
-  query GetEntries {
-    Entries {
+  query GetEntries
+  {
+    Entries{
       client
       project
       projectCode
@@ -69,74 +64,52 @@ const GET_ENTRIES = gql`
 `;
 
 const rows = [
-  createData("CLOB Rearchitecture", "Twitri", 208.0, 208.0, 0),
-  createData("Roonder 3.0", "Roonder Technologies LLC", 66.5, 66.5, 4.3),
-  createData("Business Development", "Prosaria", 41.0, 41.0, 0),
-  createData("Pharos", "Olith", 349.5, 0.0, 0),
+  createData('CLOB Rearchitecture', 'Twitri', 208.00, 208.00, 0),
+  createData('Roonder 3.0', 'Roonder Technologies LLC', 66.50, 66.50, 4.3),
+  createData('Business Development', 'Prosaria', 41.00, 41.00, 0),
+  createData('Pharos', 'Olith', 349.50, 0.00, 0)
 ];
 
 export default function BasicTable() {
   const classes = useStyles();
-  const { loading, data, error } = useQuery<EntriesData>(GET_ENTRIES);
-  if (loading) return <div>loading</div>;
-  if (error) return <div>error</div>;
-  if (data) {
-    return (
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className={classes.tableHeader}>Name</TableCell>
-              <TableCell className={classes.tableHeader}>Clients</TableCell>
-              <TableCell className={classes.tableHeader} align="right">
-                Hours
+  const { loading, data, error } = useQuery<EntriesData>(
+    GET_ENTRIES
+  );
+  if(loading) return <div>loading</div>
+  if(error) return <div>error</div>
+  if(data) return <div>data</div>
+    // const { entries } = data
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.tableHeader}>Name</TableCell>
+            <TableCell className={classes.tableHeader}>Clients</TableCell>
+            <TableCell className={classes.tableHeader} align='right'>Hours</TableCell>
+            <TableCell className={classes.tableHeader} align='right'>Billiable Hours</TableCell>
+            <TableCell className={classes.tableHeader} align='right'>Billable Amount</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell className={classes.tableCell} component="th" scope="row">
+                {row.name}
               </TableCell>
-              <TableCell className={classes.tableHeader} align="right">
-                Billiable Hours
+              <TableCell className={classes.tableCell}>{row.clients}</TableCell>
+              <TableCell className={classes.tableCell} align='right'>{row.hours}</TableCell>
+              <TableCell align='right'>
+                <div style={{display:'flex', justifyContent: 'flex-end'}}>
+                  <div>{row.billableHours}</div>
+                  <div style={{marginLeft:'4px', color:'grey', minWidth:'46px'}}>{ row.billableHours===0?'(0)$':'(100%)'}</div>
+                </div>
               </TableCell>
-              <TableCell className={classes.tableHeader} align="right">
-                Billable Amount
-              </TableCell>
+              <TableCell align='right'>{row.billableAmount>0?`$${row.billableAmount}`: '-'}</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell
-                  className={classes.tableCell}
-                  component="th"
-                  scope="row"
-                >
-                  {row.name}
-                </TableCell>
-                <TableCell className={classes.tableCell}>
-                  {row.clients}
-                </TableCell>
-                <TableCell className={classes.tableCell} align="right">
-                  {row.hours}
-                </TableCell>
-                <TableCell align="right">
-                  <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                    <div>{row.billableHours}</div>
-                    <div
-                      style={{
-                        marginLeft: "4px",
-                        color: "grey",
-                        minWidth: "46px",
-                      }}
-                    >
-                      {row.billableHours === 0 ? "(0)$" : "(100%)"}
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell align="right">
-                  {row.billableAmount > 0 ? `$${row.billableAmount}` : "-"}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
-  }
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
